75932ab92f5de9e7b38a8ba4ec1550f593b2e0dd
Saurav <sauravc41@gmail.com>
2020-03-16 23:43:41 -0700

    Switched to using PrepareStatements

--- src/Main/Menu.java
+++ src/Main/Menu.java
@@ -1,14 +1,26 @@
+package Main;
+
 import java.util.Scanner;
 import java.sql.*;
 
 public class Menu {
     public static String dbURL = "jdbc:derby://localhost:1527/JDBC Project";
-    private static String tableName = "WRITINGGROUP";
+    //private static String tableName = "";
     // jdbc Connection
     private static Connection conn = null;
     private static Statement stmt = null;
-
+    private static PreparedStatement pstmt;
+    
     public static void main(String[] args) {
+        //Start the connection to database
+        try {
+            conn = DriverManager.getConnection(dbURL);
+            //stmt = conn.createStatement();
+        }
+        catch (SQLException sqlExcept) {
+            sqlExcept.printStackTrace();
+        }
+        
         int choice;
         Scanner scan = new Scanner(System.in);
 
@@ -25,30 +37,58 @@ public class Menu {
 
         choice = scan.nextInt();
         
-        
+        if (choice == 1) {
+            try {
+                String st = "select GROUPNAME, HEADWRITER, YEARFORMED, SUBJECT from WRITINGGROUP";
+                pstmt = conn.prepareStatement(st);
+                ResultSet results = pstmt.executeQuery();
+                ResultSetMetaData rsmd = results.getMetaData();
+                
+                printTable(results, rsmd);
+                
+                results.close();
+                pstmt.close();
+            }
+            catch (SQLException sqlExcept) {
+                sqlExcept.printStackTrace();
+            }
+        }
+        if (choice == 2) {
+            try {
+                System.out.println("Which Writing Group would you like to see?");
+                String input = scan.next();
+                
+                String st = "SELECT GroupName, HeadWriter, YearFormed, Subject FROM WRITINGGROUP WHERE GroupName = ?";
+                pstmt = conn.prepareStatement(st);
+                pstmt.setString(1, input);
+                
+                ResultSet results = pstmt.executeQuery();
+                ResultSetMetaData rsmd = results.getMetaData();
+                
+                printTable(results, rsmd);
+                results.close();
+                pstmt.close();
+            }
+            catch (SQLException sqlExcept) {
+                sqlExcept.printStackTrace();
+            }
+        }
+    }
+    public static void printTable(ResultSet results, ResultSetMetaData rsmd) {
         try {
-            conn = DriverManager.getConnection(dbURL);
-            stmt = conn.createStatement();
-            ResultSet results = stmt.executeQuery("select * from " + tableName);
-            ResultSetMetaData rsmd = results.getMetaData();
             int col = rsmd.getColumnCount();
             for (int i = 1; i <= col; i++) {
                 //print Column Names
                 System.out.print(rsmd.getColumnLabel(i)+"\t\t");
             }
-
-            System.out.println("\n-------------------------------------------------");
-
-            while(results.next())
-            {
+            System.out.println("\n--------------------------------------------------------------------------------");
+            while (results.next()) {
                 String groupName = results.getString(1);
                 String headWriter = results.getString(2);
                 String yearFormed = results.getString(3);
                 String subject = results.getString(4);
                 System.out.println(groupName + "\t\t" + headWriter + "\t\t" + yearFormed + "\t\t" + subject);
             }
-            results.close();
-            stmt.close();
         }
         catch (SQLException sqlExcept) {
             sqlExcept.printStackTrace();
