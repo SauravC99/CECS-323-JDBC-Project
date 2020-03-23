22737235ce289348d608524a9b13cb357bde0184
Saurav <sauravc41@gmail.com>
2020-03-22 21:29:16 -0700

    More error checking

--- src/Main/CECS323JavaTermProject.java
+++ src/Main/CECS323JavaTermProject.java
@@ -39,8 +39,27 @@ public class CECS323JavaTermProject {
             System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(dbURL);
         }
-        catch (SQLException sqlExcept) {
-            sqlExcept.printStackTrace();
+        catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
         }
         
         int choice;
